a36fe5defb4d910bd1fec84e9ed39e9d9f86786e
MogilaVladik <sasha.vlad.mog@yandex.ru>
2025-10-28 13:20:18 +0300

    Delete onlook

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,4 @@
 import type { Metadata } from "next";
-import Script from "next/script";
 import "./globals.css";
 
 export const metadata: Metadata = {
@@ -23,13 +22,6 @@ export default function RootLayout({
     <html lang="ru">
       <body className="antialiased">
         {children}
-
-        <Script
-          src="https://cdn.jsdelivr.net/gh/onlook-dev/onlook@d3887f2/apps/web/client/public/onlook-preload-script.js"
-          strategy="afterInteractive"
-          type="module"
-          id="onlook-preload-script"
-        ></Script>
       </body>
     </html>
   );
