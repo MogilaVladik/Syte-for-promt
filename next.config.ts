c0e635901884155428e8b5f3b879ae5eda89f467
Vladislav <sasha.vlad.mog@yandex.ru>
2026-02-08 19:46:25 +0300

    pereezd

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: "export",
+  trailingSlash: true,
   devIndicators: false,
   eslint: {
     ignoreDuringBuilds: false,
@@ -9,6 +11,7 @@ const nextConfig: NextConfig = {
     ignoreBuildErrors: false,
   },
   images: {
+    unoptimized: true,
     formats: ["image/avif", "image/webp"],
     deviceSizes: [640, 750, 828, 1080, 1200, 1920],
     imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
