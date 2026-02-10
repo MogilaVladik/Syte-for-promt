d58a970f6871cdbacad30564cabb418daef446a5
Vladislav <sasha.vlad.mog@yandex.ru>
2026-02-10 23:53:10 +0300

    123414цйу

--- src/app/components/Program.tsx
+++ src/app/components/Program.tsx
@@ -15,7 +15,7 @@ export default function Program() {
         className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8"
       >
         <div
-          className="max-w-4xl"
+          className="grid lg:grid-cols-2 gap-8 lg:gap-12 items-start"
         >
           {/* Left: Content */}
           <div>
